eb9f3ed3b9bbef7f194f31b4b4bfa3bdfe9667ec
Fredy Montaña <fredy8462@hotmail.com>
2022-05-24 04:37:28 -0500

    Se crea el ejercicio correspondiente a la colección personalizaday se agregan archivos modificados

---
+++ CollectionCustom/src/main/java/entities/CollectionCustom.java
@@ -0,0 +1,13 @@
+package entities;
+
+import java.util.Collection;
+
+public class CollectionCustom {
+
+    private String[] name;
+    private String[] lastName;
+
+    public int validateSize(Collection<String> name, Collection<String> lastName) {
+        return 0;
+    }
+}
