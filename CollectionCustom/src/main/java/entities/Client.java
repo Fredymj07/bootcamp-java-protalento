e1c891d0059aaf78690cc9e839485b534e2c3aba
Fredy Montaña <fredy8462@hotmail.com>
2022-05-24 06:00:58 -0500

    Se crea la versión inicial de la clase correspondiente a la colección personalizada

---
+++ CollectionCustom/src/main/java/entities/Client.java
@@ -0,0 +1,61 @@
+package entities;
+
+public class Client {
+
+    public String nombre;
+    public String apellido;
+    public byte edad;
+    public String tipoDocumento;
+    public String numeroDocumento;
+
+    public Client() {
+    }
+
+    public Client(String nombre, String apellido, byte edad, String tipoDocumento, String numeroDocumento) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.tipoDocumento = tipoDocumento;
+        this.numeroDocumento = numeroDocumento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public byte getEdad() {
+        return edad;
+    }
+
+    public void setEdad(byte edad) {
+        this.edad = edad;
+    }
+
+    public String getTipoDocumento() {
+        return tipoDocumento;
+    }
+
+    public void setTipoDocumento(String tipoDocumento) {
+        this.tipoDocumento = tipoDocumento;
+    }
+
+    public String getNumeroDocumento() {
+        return numeroDocumento;
+    }
+
+    public void setNumeroDocumento(String numeroDocumento) {
+        this.numeroDocumento = numeroDocumento;
+    }
+}
--- CollectionCustom/src/main/java/entities/CollectionCustom.java
+++ CollectionCustom/src/main/java/entities/CollectionCustom.java
@@ -1,13 +1,47 @@
 package entities;
 
-import java.util.Collection;
+import java.util.LinkedList;
 
-public class CollectionCustom {
+public class CollectionCustom{
 
-    private String[] name;
-    private String[] lastName;
+    private String[] client;
 
-    public int validateSize(Collection<String> name, Collection<String> lastName) {
-        return 0;
+    /**
+     * Este método retorna el tamaño del arreglo de objetos tipo client
+     * @return
+     * @author Fredy Montaña
+     */
+    public int sizeCollection() {
+        return client.length;
+    }
+
+    /**
+     * Este método permite agregar un objeto client al inicio del arreglo
+     * @param client
+     * @author Fredy Montaña
+     */
+    public void addFirst(String client) {
+        LinkedList clients = new LinkedList();
+        clients.addFirst(client);
+    }
+
+    /**
+     * Este método permite agregar un objeto client al final del arreglo
+     * @param client
+     * @author Fredy Montaña
+     */
+    public void addLast(String client) {
+        LinkedList clients = new LinkedList();
+        clients.addLast(client);
+    }
+
+    /**
+     * Este método permite agregar un elemento al arreglo
+     * @param client
+     * @author Fredy Montaña
+     */
+    public void addObject(String client) {
+        LinkedList clients = new LinkedList();
+        clients.add(client);
     }
 }
